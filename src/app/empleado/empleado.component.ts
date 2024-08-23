e37d95620f5854a8d0326a6946a249a606f375b4
Lucas Prado <83882214+Prado3@users.noreply.github.com>
2024-08-23 14:38:27 -0300

    tercero

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -18,12 +18,25 @@ export class EmpleadoComponent implements OnInit {
 
   //llamaEmpresa(value:String){}
 
-  habilitacionCuadro=true;
+  habilitacionCuadro=false;
 
   usuRegistrado=false;
 
+  textoDeRegistro="No hay nadie registrado";
+
   getRegistroUsuario(){
-    this.usuRegistrado=true;
+    this.usuRegistrado=false;
+  }
+
+  setUsuarioRegistrado(event :Event){
+    
+
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.textoDeRegistro="El usuario se acaba de registrar";
+    }else{
+      this.textoDeRegistro="No hay nadie registrado";
+    }
+
   }
 
   constructor(){}
